a90a208c28df0ecfc5734e02f97ede57a24f269d
Pavlova Marina <marizza.mil89@gmail.com>
2021-09-24 21:02:23 +0300

    17_Book_List_Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-function App() {
-  return (
-    <div id="main">
-      <h1>My Reading List</h1>
+import React, { Component } from 'react';
 
-    </div>
-  );
+// components
+import BookList from './components/BookList';
+
+class App extends Component {
+  render() {
+    return (
+        <div id="main">
+          <h1>My Reading List</h1>
+          <BookList />
+      </div>
+    );
+  }
 }
 
 export default App;
